2632a568370babc6557e1dbd6e390eabdbb9ecb6
hane-infratop <143814316+hane-infratop@users.noreply.github.com>
2023-10-18 14:15:48 +0000

    break アクセス制限

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,28 +4,46 @@ class BooksController < ApplicationController
   end
   
   def create
-    book = Book.new(params[:user_id])
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book.id)
+    else
+      @book = Book.new
+      @books = Book.all
+      @user = User.find(current_user.id)
+      render :index
+    end
   end
 
   def index
     @user = User.find(current_user.id)
+    @book = Book.new
     @books = Book.all
   end
 
   def show
-    @book = Book.find(params[:id])
+    @user = User.find(current_user.id)
+    @books = @user.books
   end
 
   def edit
-    @book = Book.find(params[:id])
+    book = Book.find(params[:id])
+    unless book.id == current_user.id
+      redirect_to books_path
+    end
   end
   
   def update
-    @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@book.id)
+    book = Book.find(params[:id])
+    if book.update(book_params)
+      flash[:notice] = "You have updated user successfully."
+      redirect_to book_path(book.id)
+    else
+      flash[:notice] = "errors prohibited this obj from being saved:"
+      @book = Book.find(params[:id])
+      render :edit
+    end
   end
   
   def destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,5 +9,27 @@ class UsersController < ApplicationController
   end
 
   def edit
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to books_path
+    end
+  end
+  
+  def update
+    user = User.find(params[:id])
+    
+    if user.update(user_params)
+      flash[:notice] = "You have updated user successfully."
+      redirect_to user_path(user.id)
+    else
+      @user = User.find(current_user.id)
+      render :edit
+    end
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
 end
